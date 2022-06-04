41e33e703ee0b2adfea0ddbd8fb3ec19a1387d7d
GamebP <88152063+GamebP@users.noreply.github.com>
2022-06-05 01:05:11 +0300

    Update and rename 4Menus-Arsenal.lua to --Arsenal.lua

---
+++ --Arsenal.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HonestlyDex/DexHub/main/Arsenal"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HonestlyDex/DexHub/main/Arsenal"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HonestlyDex/DexHub/main/Arsenal"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HonestlyDex/DexHub/main/Arsenal"))()
