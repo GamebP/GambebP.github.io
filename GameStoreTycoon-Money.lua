347cd0f6e404496b83371cc1e944e8353a2461cb
GamebP <88152063+GamebP@users.noreply.github.com>
2022-11-06 20:06:13 +0200

    Update GameStoreTycoon-Money.lua

--- GameStoreTycoon-Money.lua
+++ GameStoreTycoon-Money.lua
@@ -2,6 +2,10 @@ local money = 10000 -- Max money you could get
 local children = workspace.Tycoons:GetChildren()
 for i, child in ipairs(children) do
    if child.Info.Owner.Value == game.Players.LocalPlayer.Name then
-       child.Control.Money:FireServer(money)
+	   while(true)
+	   do
+		   wait(0.1)
+		   child.Control.Money:FireServer(money)
+	   end
    end
 end
